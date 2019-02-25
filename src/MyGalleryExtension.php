f97c7c931bd2fec733e6ee1ddc2ec070a1984b95
schnoog <braun.volker@gmail.com>
2019-02-25 18:47:17 +0100

    los gehts

---
+++ src/MyGalleryExtension.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Bolt\Extension\schnoog\mygallery;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * ExtensionName extension class.
+ *
+ * @author Your Name <you@example.com>
+ */
+class ExtensionNameExtension extends SimpleExtension
+{
+}
